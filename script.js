098a29e6af442c884715cd97fa6f80795129f834
Marcos Vinicius <marcao.vfo@gmail.com>
2024-01-09 17:58:46 +0900

    modifict img for

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function toggleMode() {
   const html = document.documentElement;
 
-  // if (html.classList.contains('light')) {
-  //   html.classList.remove('light');
-  // } else {
-  //   html.classList.add('light');
-  // }
-
   html.classList.toggle('light');
+
+  const img = document.querySelector('#profile img');
+
+  if (html.classList.contains('light')) {
+    img.setAttribute('src', '');
+  } else {
+    img.setAttribute('src', '');
+  }
 }
